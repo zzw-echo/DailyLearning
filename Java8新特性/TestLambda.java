2e30ff8161d4fb1776c6d85d60cf8a2457c84224
zzw <zzw.me@qq.com>
2020-02-27 14:40:47 +0800

    lambda表达式学习

---
+++ Java8新特性/TestLambda.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * Created by zhangzewen on 2020/2/27
+ */
+public class TestLambda {
+
+    //原来的匿名内部类
+    @Test
+    public void test1() {
+        Comparator<Integer> com = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        };
+        TreeSet<Integer> ts = new TreeSet<>(com);
+    }
+
+    //lambda 表达式
+    @Test
+    public void test2() {
+//        Comparator<Integer> com = (x, y) -> Integer.compare(x, y);
+        Comparator<Integer> com = Integer::compare;
+        TreeSet<Integer> ts = new TreeSet<>(com);
+    }
+}
+
