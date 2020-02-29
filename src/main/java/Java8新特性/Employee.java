3e03de59f1294430628f70a9aa69cff72db4744b
zzw <zzw.me@qq.com>
2020-02-29 21:55:39 +0800

    添加lombok注解

--- src/main/java/Java8新特性/Employee.java
+++ src/main/java/Java8新特性/Employee.java
@@ -1,19 +1,17 @@
 package Java8新特性;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by zhangzewen on 2020/2/28
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     private String name;
     private Integer age;
     private double salary;
-
-    public Employee(String name, Integer age, double salary) {
-        this.name = name;
-        this.age = age;
-        this.salary = salary;
-    }
 }
