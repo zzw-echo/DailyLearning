5dacc1da94dddbd67271692f17baa8e3b16f9fd4
zzw <zzw.me@qq.com>
2020-03-01 23:10:16 +0800

    lambda语法

--- src/main/java/Java8新特性/TestLambda2.java
+++ src/main/java/Java8新特性/TestLambda2.java
@@ -3,7 +3,11 @@ package Java8新特性;
 /**
  * Created by zhangzewen on 2020/3/1
  *
- * 一、Lambda表达式的基础语法：Java8中引入了一个新的操作符“->”
+ * 一、Lambda 表达式的基础语法：Java8 中引入了一个新的操作符"->" 该操作符称为箭头操作符或Lambda 操作符
+ * Lambda表达式就是对接口的实现
+ *
+ * 左侧：Lambda表达式的参数列表
+ * 右侧：Lambda表达式中需要执行的功能，即Lambda体
  */
 public class TestLambda2 {
 }
