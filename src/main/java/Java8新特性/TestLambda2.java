ca216a2e59ca1beb9838ce3d75c73c0106a2e964
zzw <zzw.me@qq.com>
2020-03-02 11:54:01 +0800

    lambda语法格式

--- src/main/java/Java8新特性/TestLambda2.java
+++ src/main/java/Java8新特性/TestLambda2.java
@@ -1,5 +1,7 @@
 package Java8新特性;
 
+import org.junit.Test;
+
 /**
  * Created by zhangzewen on 2020/3/1
  *
@@ -8,6 +10,25 @@ package Java8新特性;
  *
  * 左侧：Lambda表达式的参数列表
  * 右侧：Lambda表达式中需要执行的功能，即Lambda体
+ *
+ * 语法格式一：无参数，无返回值
+ *      () -> System.out.println("hello Lambda! ")
+ *
  */
 public class TestLambda2 {
+
+    @Test
+    public void test1(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello Lambda! ");
+            }
+        };
+
+        System.out.println("---------------------");
+
+        Runnable runnable1 = () -> System.out.println("hello Lambda! 1  ");
+    }
+
 }
