dd2b694e0c61b20377ca5ebe1bcc625c28a93277
zzw <zzw.me@qq.com>
2020-03-03 23:40:55 +0800

    Java8内置四大函数式接口

--- src/main/java/Java8新特性/TestLambda3.java
+++ src/main/java/Java8新特性/TestLambda3.java
@@ -3,9 +3,11 @@ package Java8新特性;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**
@@ -66,12 +68,38 @@ public class TestLambda3 {
 
     @Test
     public void test3() {
-        String newStr = strHandler("\t\t\t  去除  空格", (str) -> str.trim());
+        String newStr = strHandler("\t\t\t  去除  空格", s -> s.trim());
         System.out.println(newStr);
 
         String subStr = strHandler("这是截取前的原字符串", (str) -> str.substring(2, 5));
         System.out.println(subStr);
     }
+
+
+    //Predicate<T> 断言型接口
+    @Test
+    public void test4(){
+        List<String> list = Arrays.asList("Hello", "wfq", "echo","ok", "This is test ..");
+        List<String> strList = filterStr(list, s -> s.length() > 3);
+        for (String s : strList) {
+            System.out.println(s);
+        }
+    }
+
+
+    //需求：将满足条件的字符串，放入集合中
+    public List<String> filterStr(List<String> list, Predicate<String> pre){
+        List<String> strList = new ArrayList<>();
+        for (String str : list) {
+            if (pre.test(str)) {
+                strList.add(str);
+            }
+        }
+        return strList;
+    }
+
+
+
 
 
 }
