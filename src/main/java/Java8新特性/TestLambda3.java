ea4f57349e316dadd3399a2164efefddf7b20ed0
zzw <zzw.me@qq.com>
2020-03-03 16:55:36 +0800

    Java8内置四大函数式接口

---
+++ src/main/java/Java8新特性/TestLambda3.java
@@ -0,0 +1,22 @@
+package Java8新特性;
+
+/**
+ * Created by zhangzewen on 2020/3/3
+ *
+ * Java8 内置的四大核心函数式接口
+ *
+ * Consumer<T t>: 消费型接口
+ * void accept(T t);
+ *
+ * Supplier<T t>: 供给型接口
+ * T get();
+ *
+ * Function(T, R): 函数型接口
+ * R apply(T t);
+ *
+ * Predicate(T): 断言型接口
+ * boolean test(T t);
+ *
+ */
+public class TestLambda3 {
+}
