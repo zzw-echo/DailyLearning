ac0b08807fd7852b81cee523b84af7573e05eeff
zzw <zzw.me@qq.com>
2020-03-09 21:49:48 +0800

    StreamAPI  筛选与切片

---
+++ src/main/java/Java8新特性/TestStreamAPI2.java
@@ -0,0 +1,42 @@
+package Java8新特性;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by zhangzewen on 2020/3/9
+ */
+public class TestStreamAPI2 {
+
+    List<Employee> employees = Arrays.asList(
+            new Employee("张三", 18, 111.11),
+            new Employee("李四", 35, 222.22),
+            new Employee("王五", 36, 333.33),
+            new Employee("赵六", 45, 444.44),
+            new Employee("田七", 66, 555.55)
+    );
+
+    //中间操作
+    /*
+        筛选与切片
+        filter
+        limit
+        skip(n)
+        distinct
+     */
+    @Test
+    public void test1() {
+        //中间操作(延迟加载)
+        Stream<Employee> stream = employees.stream().filter(e -> {
+            System.out.println("this is middle operation of Stream API .. ");
+            return e.getAge() > 35;
+        });
+
+        //终止操作
+        stream.forEach(System.out::println);
+    }
+
+}
