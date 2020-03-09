663e1667c46208bbd79f57ebd86882613e383b9e
zzw <zzw.me@qq.com>
2020-03-09 16:31:57 +0800

    StreamAPI

---
+++ src/main/java/Java8新特性/BiFunction.java
@@ -0,0 +1,8 @@
+package Java8新特性;
+
+/**
+ * Created by zhangzewen on 2020/3/9
+ */
+public interface BiFunction<T1,T2,R> {
+    R apply(T1 t1, T2 t2);
+}
---
+++ src/main/java/Java8新特性/TestStreamAPI1.java
@@ -0,0 +1,36 @@
+package Java8新特性;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by zhangzewen on 2020/3/9
+ */
+public class TestStreamAPI1 {
+
+    //创建Stream
+    @Test
+    public void test1(){
+        //1. Collection 系列集合提供的Stream() 或者parallelStream()
+        List<String> list = new ArrayList<>();
+        Stream<String> stream1 = list.stream();
+
+        //2. 通过Arrays 中的静态方法Stream() 获取数组流
+        Employee[] emp = new Employee[10];
+        Stream<Employee> stream2 = Arrays.stream(emp);
+
+        //3. 通过Stream 类中的静态方法of()
+        Stream<String> stream3 = Stream.of("aa", "bb", "cc");
+
+        //4.创建无限流  迭代
+        Stream<Integer> stream4 = Stream.iterate(0, x -> x + 2);
+
+
+
+    }
+
+}
