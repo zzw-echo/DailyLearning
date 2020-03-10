0697700756f0632b837893079656fb3007215671
zzw <zzw.me@qq.com>
2020-03-10 22:15:09 +0800

    StreamAPI--终止操作-查找与匹配

--- src/main/java/Java8新特性/StreamAPI3.java
+++ src/main/java/Java8新特性/StreamAPI3.java
@@ -1,7 +1,11 @@
 package Java8新特性;
 
+import org.junit.Test;
+
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by zhangzewen on 2020/3/10
@@ -9,9 +13,7 @@ import java.util.List;
  * StreamAPI终止操作
  */
 public class StreamAPI3 {
-    /*
-        查找与匹配
-     */
+
     List<Employee> employees = Arrays.asList(
             new Employee("张三", 18, 111.11, Employee.Status.BUSY),
             new Employee("李四", 35, 222.22, Employee.Status.FREE),
@@ -22,6 +24,39 @@ public class StreamAPI3 {
             new Employee("田七", 66, 555.55, Employee.Status.BUSY),
             new Employee("田七", 66, 555.55, Employee.Status.BUSY)
     );
+
+    /*
+        查找与匹配
+     */
+    @Test
+    public void test1(){
+        boolean b = employees.stream().allMatch(e -> e.getStatus().equals(Employee.Status.BUSY));
+        System.out.println(b);
+
+        boolean b1 = employees.stream().anyMatch(e -> e.getStatus().equals(Employee.Status.BUSY));
+        System.out.println(b1);
+
+        boolean b2 = employees.stream()
+                .noneMatch(employee -> employee.getStatus().equals(Employee.Status.FREE));
+        System.out.println(b2);
+
+        employees.stream()
+                .sorted((e1,e2)->Double.compare(e1.getSalary(),e2.getSalary())).findFirst();
+        Optional<Employee> optional = employees.stream()
+                .sorted(Comparator.comparingDouble(Employee::getSalary)).findFirst();
+        System.out.println(optional.get());
+
+        Optional<Employee> optional1 = employees.stream()
+                .filter(e -> e.getStatus().equals(Employee.Status.FREE)).findAny();
+        System.out.println(optional1.get());
+
+        Optional<Employee> optional2 = employees.parallelStream()
+                .filter(e -> e.getStatus().equals(Employee.Status.FREE)).findAny();
+        System.out.println(optional2.get());
+
+    }
+
+
 
 
 }
