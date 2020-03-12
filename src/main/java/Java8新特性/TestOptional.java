18f0c1a5a413c2d214ac401b78971c97f10002ee
zzw <zzw.me@qq.com>
2020-03-12 15:16:56 +0800

    Optional类

--- src/main/java/Java8新特性/TestOptional.java
+++ src/main/java/Java8新特性/TestOptional.java
@@ -1,7 +1,19 @@
 package Java8新特性;
 
+import org.junit.Test;
+
+import java.util.Optional;
+
 /**
  * Created by zhangzewen on 2020/3/12
  */
 public class TestOptional {
+
+    @Test
+    public void test1(){
+        Optional<Employee> op = Optional.of(new Employee());
+        Employee employee = op.get();
+        System.out.println(employee);
+
+    }
 }
