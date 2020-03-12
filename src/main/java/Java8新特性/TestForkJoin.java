c142b0d1a1781ec4a21edd4b9078304357a32f13
zzw <zzw.me@qq.com>
2020-03-12 14:59:37 +0800

    并行流与串行流-forkjoin

--- src/main/java/Java8新特性/TestForkJoin.java
+++ src/main/java/Java8新特性/TestForkJoin.java
@@ -7,15 +7,14 @@ import java.util.concurrent.ForkJoinTask;
 
 /**
  * Created by zhangzewen on 2020/3/12
- *
+ * <p>
  * 线程状态
  * NEW, RUNNABLE, RUNNING, BLOCKED, DEAD
- *
  */
 public class TestForkJoin {
 
     @Test
-    public void test1(){
+    public void test1() {
 
         ForkJoinPool pool = new ForkJoinPool();
         ForkJoinTask<Long> task = new ForkJoinCalculate(0, 100000000L);
@@ -24,6 +23,15 @@ public class TestForkJoin {
 
     }
 
+    @Test
+    public void test2() {
+        long sum = 0L;
+        for (long i = 0; i < 100000000L; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+    }
 
 
 }
