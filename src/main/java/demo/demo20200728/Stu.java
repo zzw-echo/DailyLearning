6008dd0d2f5b82f391afa14df810bbda2ae94c43
zzw <zzw.me@qq.com>
2020-07-29 00:14:20 +0800

    2020/07/28

---
+++ src/main/java/demo/demo20200728/Stu.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangzewen on 2020/7/29
+ */
+public class Stu implements Cloneable {
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ src/main/java/demo/demo20200728/浅拷贝深拷贝.java
@@ -0,0 +1,15 @@
+/**
+ * Created by zhangzewen on 2020/7/29
+ */
+public class 浅拷贝深拷贝 {
+    public static void main(String[] args) {
+
+        Stu stu = new Stu();
+
+
+    }
+
+
+
+
+}
