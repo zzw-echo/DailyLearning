a55b392611110dcc20c4cffb6329a205b26df2ac
zzw-echo <zzw.me@qq.com>
2020-08-06 00:13:23 +0800

    2020/08/05

--- src/main/java/demo/demo0805/shopee/InvalidTransactions.java
+++ src/main/java/demo/demo0805/shopee/InvalidTransactions.java
@@ -1,26 +1,22 @@
-package demo.demo0805;
+package demo.demo0805.shopee;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @ Author     ：zhangzewen
- * @ Date       ：Created in 2020/8/5 15:27
+ * @ Date       ：Created in 2020/8/5 23:41
  * @ Description：
  */
-public class demo4 {
+public class InvalidTransactions {
 
-    /**
-     * @param vTransaction string字符串一维数组
-     * @return string字符串一维数组
-     */
     public String[] invalidTransaction(String[] vTransaction) {
-        // write code here
         Transaction[] trans = new Transaction[vTransaction.length];
-        int index = 0;
-        for (String tran : vTransaction) {
+        for (int i = 0; i < vTransaction.length; i++) {
+            String tran = vTransaction[i];
             String[] strings = tran.split(",");
-            trans[index++] = new Transaction(strings[0], Integer.parseInt(strings[1]), Integer.parseInt(strings[2]), strings[3]);
+            trans[i] = new Transaction(strings[0], Integer.parseInt(strings[1]), Integer.parseInt(strings[2]), strings[3]);
         }
         List<String> list = new ArrayList<>();
         for (int i = 0; i < trans.length; i++) {
@@ -28,20 +24,19 @@ public class demo4 {
                 list.add(vTransaction[i]);
             } else {
                 for (int j = 0; j < trans.length; j++) {
-                    if (i != j && trans[j].name.equals(trans[i].name) && !trans[j].city.equals(trans[i].city) && Math.abs(trans[j].time - trans[i].time) <= 60) {
+                    if (i != j && trans[i].name.equals(trans[j].name) && !trans[i].city.equals(trans[j].city)
+                            && Math.abs(trans[i].time - trans[j].time) <= 60) {
                         list.add(vTransaction[i]);
                         break;
                     }
                 }
             }
         }
-
-        String[] result = new String[list.size()];
+        String[] results = new String[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+            results[i] = list.get(i);
         }
-
-        return result;
+        return results;
     }
 
     public class Transaction {
@@ -57,4 +52,6 @@ public class demo4 {
             this.city = city;
         }
     }
+
 }
+
