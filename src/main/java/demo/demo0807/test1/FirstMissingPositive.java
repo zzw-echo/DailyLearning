5d0c8b7e6250d32eab23a884fc13e733a59c5197
zzw-echo <zzw.me@qq.com>
2020-08-07 10:51:37 +0800

    2020/08/07

---
+++ src/main/java/demo/demo0807/test1/FirstMissingPositive.java
@@ -0,0 +1,54 @@
+package demo.demo0807.test1;
+
+/**
+ * @ Author     ：zhangzewen
+ * @ Date       ：Created in 2020/8/6 13:16
+ * @ Description：
+ */
+public class FirstMissingPositive {
+
+    //[1, n+1]
+    //1、置换
+    //nums[i] ==> nums[nums[i] - 1]
+
+    //2、hash
+
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public int firstMissingPositive2(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                int x = Math.abs(nums[i]);
+                nums[x] = -Math.abs(nums[x]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
