166bf56605ad5228c1071b574e2aafc0b5a43fd2
zzw-echo <zzw.me@qq.com>
2020-08-14 13:50:43 +0800

    b站笔试题

--- src/main/java/demo/demo0813/demo1.java
+++ src/main/java/demo/demo0813/demo1.java
@@ -11,6 +11,16 @@ import java.util.List;
  */
 public class demo1 {
 
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(2);
+        list.add(1);
+        list.add(10);
+        System.out.println(list.size());
+        list.remove(0);
+        System.out.println(list.size());
+    }
 
     public boolean Game24Points(int[] arr) {
         List<Integer> list = new ArrayList<>();
