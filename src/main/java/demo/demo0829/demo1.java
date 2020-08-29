f0f29867373591f2ddc712838708e4783e12b06e
zzw-echo <zzw.me@qq.com>
2020-08-29 22:02:58 +0800

    renew

---
+++ src/main/java/demo/demo0829/demo1.java
@@ -0,0 +1,16 @@
+package demo.demo0829;
+
+import java.math.BigDecimal;
+
+/**
+ * 作者 ： 张泽文
+ * 邮箱 ： zzw.me@qq.com
+ * 时间 ： 2020/8/29 19:23
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        BigDecimal bd1 = new BigDecimal(Double.toString(0.1));
+        BigDecimal bd2 = new BigDecimal(Double.toString(0.2));
+        System.out.println(bd1.add(bd2));
+    }
+}
