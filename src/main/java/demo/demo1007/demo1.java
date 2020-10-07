abbf55d4e7d6643bbc93c9d8fb5a1d747fa55450
zzw-echo <zzw.me@qq.com>
2020-10-07 20:59:29 +0800

    renew

--- src/main/java/demo/demo1007/demo1.java
+++ src/main/java/demo/demo1007/demo1.java
@@ -9,24 +9,29 @@ import java.math.BigDecimal;
  */
 public class demo1 {
 
-    public static double myAdd(double v1, double v2){
-        BigDecimal b1 = new BigDecimal(v1);
-
-
-        System.out.println(b1.doubleValue());
-
-
-        return 0;
-    }
 
     public static void main(String[] args) {
 
-        double d1 = 0.1;
-        BigDecimal b1 = new BigDecimal(Double.toString(d1));
+        double myD1 = 0.1;
+        BigDecimal myB1 = new BigDecimal(Double.toString(myD1));
 
+        BigDecimal a = new BigDecimal("0.1");
+        BigDecimal b = new BigDecimal("0.2");
 
+        BigDecimal add = a.add(b);
+        BigDecimal subtract = a.subtract(b);
+        BigDecimal multiply = a.multiply(b);
+        BigDecimal divide = a.divide(b);
+        BigDecimal divide2 = a.divide(b).setScale(2, BigDecimal.ROUND_HALF_DOWN);
+
+        int compareTo = a.compareTo(b);
 
     }
 
+    public static double myAdd(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(v1);
+        System.out.println(b1.doubleValue());
+        return 0;
+    }
 
 }
