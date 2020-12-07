343073258fba4047c783f3432bddc8a52324de04
zzw-echo <zzw.me@qq.com>
2020-12-07 13:54:17 +0800

    renew

--- src/main/java/demo/demo1207/q1.java
+++ src/main/java/demo/demo1207/q1.java
@@ -21,14 +21,15 @@ public class q1 {
         return new int[0];
     }
 
-    public int[] twoSum2(int[] nums, int target){
+    public int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(target - nums[i])){
-                return new int[]{hashMap.get(target - nums[i]),i};
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
             }
-            hashMap.put(nums[i],i);
+            hashMap.put(nums[i], i);
         }
+        return new int[0];
 
     }
 }
